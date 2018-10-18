f92ccd75e691c619ceb1a8ec72b98a8bb97899c9
S_A <16127909+SA3291@users.noreply.github.com>
2018-10-18 17:44:30 -0400

    Add scripts to ssh model
    - for both pgm and ne
    - also fixes the results gdx file name

--- code/scripts/run_cge_pgm.sh
+++ code/scripts/run_cge_pgm.sh
@@ -3,4 +3,4 @@ ssh TheWorkers@172.19.138.112 '
 export PATH=$PATH:"/Applications/GAMS25.1/GAMS Terminal.app/../sysdir" && cd ~/Documents/Saketh
 gams stdcge_beccs_pgm.gms'
 scp TheWorkers@172.19.138.112:Documents/Saketh/stdcge_beccs_pgm.lst ../model/stdcge_beccs_pgm.lst 
-scp TheWorkers@172.19.138.112:Documents/Saketh/results.gdx ../../data/cge/results.gdx 
+scp TheWorkers@172.19.138.112:Documents/Saketh/results.gdx ../../data/cge/results_pgm.gdx 
