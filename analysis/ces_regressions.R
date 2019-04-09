d7c190133723a9dedb4f07d6359df7244f564ae4
S_A <16127909+SA3291@users.noreply.github.com>
2019-04-09 18:44:12 -0400

    Update CES regressions
    - add IV fixed effects regressions
    - update stargazer output

--- analysis/ces_regressions.R
+++ analysis/ces_regressions.R
@@ -5,23 +5,35 @@ library(stargazer)
 library(commarobust)
 library(ivpack)
 library(sandwich)
+library(plm)
 
+## Import DAta
+
+# Set working directory to script location
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 reg_data <- read_csv('../data/processed/ces_monthly.csv')
 reg_data$state <- factor(reg_data$state_1)
 
-## Regressions
+# Adjust degree day scale
+reg_data$CDD_1 = reg_data$CDD_1/1000
+reg_data$CDD_2 = reg_data$CDD_2/1000
+reg_data$HDD_1 = reg_data$HDD_1/1000
+reg_data$HDD_2 = reg_data$HDD_2/1000
 
-# Linear Models
+## Linear Models
+
+# Regressions
+
 reg_lm_1   = lm(paste0('ln_load_rel ~ ln_price_rel'),
                 data = reg_data)
 
-reg_lm_2   = lm(paste0('ln_load_rel ~ (CDD_1+1)',
-                       ' + (CDD_2+1) + (HDD_1+1) + (HDD_2+1)',
+reg_lm_2   = lm(paste0('ln_load_rel ~ (CDD_1)',
+                       ' + (CDD_2) + (HDD_1) + (HDD_2)',
                        ' + ln_price_rel'),
                 data = reg_data)
 
-reg_lm_3   = lm(paste0('ln_load_rel ~ time_diff + (CDD_1+1)',
-                       ' + (CDD_2+1) + (HDD_1+1) + (HDD_2+1)',
+reg_lm_3   = lm(paste0('ln_load_rel ~ time_diff + (CDD_1)',
+                       ' + (CDD_2) + (HDD_1) + (HDD_2)',
                        ' + ln_price_rel'),
                 data = reg_data)
 
@@ -32,53 +44,30 @@ reg_plm_2  = plm(as.formula(paste0('ln_load_rel ~ ln_price_rel + (CDD_1)',
                                    ' + (CDD_2) + (HDD_1) + (HDD_2)')),
                  index = 'state', model = 'within', data = reg_data)
 
-reg_plm_3  = plm(as.formula(paste0('ln_load_rel ~ ln_price_rel + (CDD_1+1)',
-                                   ' + (CDD_2+1) + (HDD_1+1)',
-                                   ' + (HDD_2+1) + time_diff')),
+reg_plm_3  = plm(as.formula(paste0('ln_load_rel ~ ln_price_rel + (CDD_1)',
+                                   ' + (CDD_2) + (HDD_1)',
+                                   ' + (HDD_2) + time_diff')),
                  index = 'state', model = 'within', data = reg_data)
 
-# IV Regressions
+# Stargazer
 
-ivreg_1 <- ivreg(paste0('ln_load_rel ~ time_diff  + log(CDD_1+1)',
-                      ' + log(CDD_2+1) + log(HDD_1+1) + log(HDD_2+1)',
-                      ' + ln_price_rel | . - ln_price_rel + ln_coal_rel'),
-               data = reg_data)
-
-ivreg_2 <- ivreg(paste0('ln_load_rel ~  factor(state_1) + time_diff +',
-                      ' + log(CDD_1+1)  + log(CDD_2+1) + log(HDD_1+1)',
-                      ' + log(HDD_2+1) + ln_price_rel',
-                      ' | . - ln_price_rel + ln_coal_rel'),
-               data = reg_data)
-
-
-
-## Stargazer
-
-# OLS
 fits       = list(reg_lm_1, reg_lm_2, reg_lm_3,
                   reg_plm_1, reg_plm_2, reg_plm_3)
 
-robust_ses = list(coeftest(reg_lm_1,  vcovHC)[,2],
-                  coeftest(reg_lm_2,  vcovHC)[,2],
-                  coeftest(reg_lm_3,  vcovHC)[,2],
-                  coeftest(reg_plm_1, vcovHC)[,2],
-                  coeftest(reg_plm_2, vcovHC)[,2],
-                  coeftest(reg_plm_3, vcovHC)[,2])
+robust_ses = lapply(fits, function(x) {coeftest(x, vcovHC)[,2]})
 
-robust_ps  = list(coeftest(reg_lm_1,  vcovHC)[,4],
-                  coeftest(reg_lm_2,  vcovHC)[,4],
-                  coeftest(reg_lm_3,  vcovHC)[,4],
-                  coeftest(reg_plm_1, vcovHC)[,4],
-                  coeftest(reg_plm_2, vcovHC)[,4],
-                  coeftest(reg_plm_3, vcovHC)[,4])
+robust_ps = lapply(fits, function(x) {coeftest(x, vcovHC)[,4]})
 
 extra_lines = list(
     c('State FEs', ' ', ' ', ' ', 'Yes', 'Yes', 'Yes')
 )
 
+
 stargazer(fits,
           type = 'latex',
-          dep.var.labels = c('test', 'test', 'test', 'test', 'test', 'test'),
+          covariate.labels =  c('Delta_{t,s}', 'CDD_t', 'CDD_s',
+                             'HDD_t', 'HDD_s', 'ln (P_{t,i} / P_{s,i})'),
+          dep.var.labels.include = FALSE,
           star.cutoffs = c(0.05, 0.01, 0.001),
           se = robust_ses,
           p  = robust_ps,
@@ -86,4 +75,75 @@ stargazer(fits,
           column.separate = c(3, 3),
           model.names = FALSE,
           omit.stat=c("ser"))
+
+
+## IV Models
+
+# Regressions
+
+reg_iv_1  = ivreg(as.formula(paste0('ln_load_rel ~ ln_price_rel ',
+                                  '| . -ln_price_rel + ln_coal_rel')),
+                data = reg_data)
+
+reg_iv_2  = ivreg(as.formula(paste0('ln_load_rel ~ (CDD_1) + (CDD_2) + (HDD_1)',
+                                  ' + (HDD_2) + ln_price_rel ',
+                                  ' | . -ln_price_rel + ln_coal_rel')),
+                data = reg_data)
+
+reg_iv_3  = ivreg(as.formula(paste0('ln_load_rel ~ time_diff + (CDD_1)',
+                                  ' + (CDD_2) + (HDD_1) + (HDD_2) ',
+                                  ' + ln_price_rel | . -ln_price_rel ',
+                                  ' + ln_coal_rel')),
+                data = reg_data)
+
+reg_data_dmd <- reg_data %>%
+    group_by(state) %>%
+    mutate_at(c('ln_price_rel', 'ln_load_rel', 'time_diff',
+                'CDD_1', 'CDD_2', 'HDD_1', 'HDD_2', 'ln_coal_rel'),
+              funs(. - mean(.)))
+
+reg_fiv_1  = ivreg(as.formula(paste0('ln_load_rel ~  ~ -1 + ln_price_rel ',
+                                    '| . -ln_price_rel + ln_coal_rel')),
+                  data = reg_data_dmd)
+
+reg_fiv_2  = ivreg(as.formula(paste0('ln_load_rel ~ (CDD_1) + (CDD_2) + (HDD_1)',
+                                    ' + (HDD_2) + ln_price_rel  ~ -1 ',
+                                    ' | . -ln_price_rel + ln_coal_rel')),
+                  data = reg_data_dmd)
+
+reg_fiv_3  = ivreg(as.formula(paste0('ln_load_rel ~ -1 + time_diff + (CDD_1)',
+                                    ' + (CDD_2) + (HDD_1) + (HDD_2) ',
+                                    ' + ln_price_rel | . -ln_price_rel ',
+                                    ' + ln_coal_rel')),
+                  data = reg_data_dmd)
+
+
+
+# Stargazer
+
+fits       = list(reg_iv_1,  reg_iv_2,  reg_iv_3,
+                  reg_fiv_1,  reg_fiv_2, reg_fiv_3)
+
+robust_ses = lapply(fits, function(x) {coeftest(x, vcovHC)[,2]})
+
+robust_ps = lapply(fits, function(x) {coeftest(x, vcovHC)[,4]})
+
+extra_lines = list(
+    c('State FEs', ' ', ' ', ' ', 'Yes', 'Yes', 'Yes')
+)
+
+stargazer(fits,
+          type = 'latex',
+          covariate.labels =  c('Delta_{t,s}', 'CDD_t', 'CDD_s',
+                                'HDD_t', 'HDD_s', 'ln (P_{t,i} / P_{s,i})'),
+          dep.var.labels.include = FALSE,
+          star.cutoffs = c(0.05, 0.01, 0.001),
+          se = robust_ses,
+          p  = robust_ps,
+          add.lines = extra_lines,
+          column.separate = c(3, 3),
+          model.names = FALSE,
+          omit.stat=c("ser"))
+
+
 
