f3c5fcf25d4f3bbac2731c3a6e6ed3d68b6e1913
S_A <16127909+SA3291@users.noreply.github.com>
2019-04-16 18:45:59 -0400

    Update simulation
    - add effects of elasticity on log difference in quantities
    - rename app

--- code/matlab/simulation_april/main.m
+++ code/matlab/simulation_april/main.m
@@ -6,7 +6,7 @@
 
 %% Parameters
 
-alpha = [1, 2];
+alpha = [0.3, 0.7];
 xi_1  = [1, 1];
 xi_2  = [0.5, 1.5];
 
@@ -32,17 +32,15 @@ Y = ((alpha'./prices).^(sigma)) * (budget/P);
 X = (xi_mat')\Y;
 
 
-%% Price Elasticity Sim
+%% Price (Cost) Elasticity Sim
 
 n = 100;
 results = zeros(n,2); 
-cost_range = linspace(0.5,1.5,n);
+cost_multiplier = linspace(0.5,1.5,n);
 
 for i = 1:n
     
-    x_1_cost_param = cost_range(i);
-
-    alpha = [1, 2];
+    alpha = [0.3, 0.7];
     xi_1  = [1, 1];
     xi_2  = [0.5, 1.5];
 
@@ -50,6 +48,7 @@ for i = 1:n
     phi   = (sigma - 1)/sigma;
     budget = 1;
 
+    x_1_cost_param = 1*cost_multiplier(i);
     x_2_cost_param = 1;
 
     % Prices
@@ -73,7 +72,7 @@ for i = 1:n
 end    
     
 output = [];
-output(1,:) = cost_range'./1;
+output(1,:) = cost_multiplier'./1;
 output(2,:) = results(:,1)./results(:,2);
 
 % subset to positive quantities
@@ -81,11 +80,63 @@ ind = ~any(output <= 0);
 output = output(:,ind);
 
 % relationship between log prices and quantities
-plot(log(output(1,:)), log(output(2,:)))
-xlabel('Log Difference in Prices')
-ylabel('Log Difference in Quantities')
+plot(log(output(2,:)), log(output(1,:)))
+ylabel('Log Difference in Prices')
+xlabel('Log Difference in Quantities')
 
     
+%% Sigma Effects Sim
+
+n = 100;
+results = zeros(n,2); 
+sigma_range = linspace(0.01,2,n);
+
+for i = 1:n
+   
+    alpha = [0.3, 0.7];
+    xi_1  = [1, 1];
+    xi_2  = [0.5, 1.5];
+
+    sigma = sigma_range(i);
+    phi   = (sigma - 1)/sigma;
+    budget = 1;
+
+    x_1_cost_param = 1;
+    x_2_cost_param = 1;
+
+    % Prices
+    xi_mat   = [xi_1; xi_2];
+    cost_mat = [x_1_cost_param; x_2_cost_param];
+    prices   = xi_mat\cost_mat;
+    
+    if any(prices<0)
+        continue
+    end
+    
+    % Price Index
+    P = ((1/2) * (prices'.^(1-sigma))*(alpha'.^sigma)).^(1/(1-sigma));
+
+    % Quantities
+    Y = ((alpha'./prices).^(sigma)) * (budget/P);
+    X = (xi_mat')\Y;
+    
+    results(i,:) = X';
+    
+end    
+    
+output = [];
+output(1,:) = sigma_range';
+output(2,:) = results(:,1)./results(:,2);
+
+% subset to positive quantities
+ind = ~any(output <= 0);
+output = output(:,ind);
+
+% relationship between log prices and quantities
+plot((output(1,:)), log(output(2,:)))
+xlabel('Elasticity of Substitution (\sigma)')
+ylabel('Log Difference in Quantities')
+
     
     
     
